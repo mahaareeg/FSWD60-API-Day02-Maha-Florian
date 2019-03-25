32e17337988601d294c3cff011887ec421d92b53
mahsa <mahmoudmaha@hotmail.com>
2019-03-25 13:44:22 +0100

    create news file

--- exc01.php
+++ exc01.php
@@ -4,8 +4,16 @@ require_once 'RESTful.php';
 $url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
 $response = curl_get($url);
 $xml = simplexml_load_string($response);
-foreach ($xml->channel->item as $item) {
-echo '<a href="'.$item->link.'" target="_blank">'.$item->title.'</a><br>';
-}
+$num = rand(0,20);
+
+echo '<a href="'.$xml->channel->item[$num]->link.'" target="_blank">'.$xml->channel->item[$num]->title.'</a><br>';
+
+
+
+$url2="http://api.serri.codefactory.live/random/";
+$req = curl_get($url2);
+$result = json_decode($req);
+echo $result->joke;
+
 
 ?>
---
+++ news.php
@@ -0,0 +1,83 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>Newspaper</title>
+
+	<link rel="stylesheet" type="text/css" href="news.css">
+
+</head>
+
+<body>
+
+
+<header>
+
+
+	<h4>
+
+	<img src="img/logo-newspaper-cf.png" /> </h4>
+	
+	<h5>
+
+	<a href="https://www.codefactory.wien"> <img src="img/banner-1.png" />
+	</h5>
+
+
+<div class="nav">
+
+<nav>
+	<ul>
+
+     <li> <a href="HOME.html"> HOME</a></li> 
+     <li> <a href="Tech.html"> Tech</a></li>
+     <li> <a href="Culture.html"> Culture</a></li>
+     <li> <a href="Video.html"> Video</a></li>
+     
+      </ul>
+      
+    </nav>
+
+</div>
+
+</header>
+
+
+
+
+
+<footer>
+	<nav>
+	<ul>
+
+     <li> <a href="HOME.html"> HOME </a> </li> 
+     <li> <a href="Tech.html"> Tech</a></li>
+     <li> <a href="Culture.html"> Culture</a></li>
+     <li> <a href="Video.html"> Video </a></li>
+     
+      </ul>
+      
+    </nav>
+
+	
+	<h6>
+	<img src="img/logo-newspaper-cf.png" /> </h6>
+	
+<p>
+Maha-CodeFactory 2018</p> 
+
+
+
+
+
+</footer>
+
+
+
+
+
+
+</body>
+</html>
